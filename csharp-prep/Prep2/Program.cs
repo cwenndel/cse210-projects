172fea30b94be2e405d66c9f4be3b1b58405a581
cwenndel <das21013@byui.edu>
2023-04-24 21:21:41 -0300

    Add project 1 and 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        
+        Console.Write("Type your grade percentage? ");
+        string grade = Console.ReadLine();
+        int gradePercentage = int.Parse(grade);
+
+        if (gradePercentage >= 90)
+        {
+            Console.WriteLine("Letter A");
+        }
+        else if (gradePercentage >=80)
+        {
+            Console.WriteLine("Letter B");
+        }
+        else if (gradePercentage >=70)
+        {
+            Console.WriteLine("Letter C");
+        }
+        else if (gradePercentage >= 60)
+        {
+            Console.WriteLine("Letter D");
+        }
+        else
+        {
+            Console.WriteLine("Letter F");
+        }
+
+        if (gradePercentage >= 70)
+        {
+            Console.WriteLine("You Passed. Congratulation!");
+        }
+        else
+        {
+            Console.WriteLine("Try again");
+        }
     }
 }
