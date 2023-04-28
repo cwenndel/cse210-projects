d4ab619a5cd83091f1d8a927dd41cedc4e9155bb
cwenndel <das21013@byui.edu>
2023-04-27 22:29:22 -0300

    Add prep3 loops

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -3,7 +3,32 @@ using System;
 class Program
 {
     static void Main(string[] args)
+
     {
-        Console.WriteLine("Hello Prep3 World!");
+
+        Console.WriteLine("What is the magic number? ");
+        var magicnumber = Console.ReadLine();
+        int number = int.Parse(magicnumber);
+
+
+        int choice = 1;
+        while (choice != number)
+        {
+            Console.WriteLine("What is your guess? ");
+            choice = int.Parse(Console.ReadLine());
+           
+            if (choice < number)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (choice > number)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("You guess it");
+            }
+        }
     }
 }
