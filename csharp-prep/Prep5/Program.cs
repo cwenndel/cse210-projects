0e6434bfead10490b069fc3a943b1912ae4821bf
cwenndel <das21013@byui.edu>
2023-04-29 23:13:28 -0300

    update

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        message();
+        var name = getname();
+        int calc = favoriteNumber();
+        {
+            Console.WriteLine($"{name}, the square of your number is {calc}");
+        }
+    }
+    static void message()
+    {
+        Console.WriteLine("Welcome to the Program");
+    }
+
+    static string getname()
+    {
+        Console.WriteLine("Please enter your name: ");
+        var name = Console.ReadLine();
+        return name;
+    }
+    static int favoriteNumber()
+    {
+        Console.WriteLine("Please enter your favorite number: ");
+        var number = int.Parse(Console.ReadLine());
+
+        var calc = (int) Math.Pow(number, 2);
+        Console.WriteLine(calc);
+        return calc;
     }
 }
